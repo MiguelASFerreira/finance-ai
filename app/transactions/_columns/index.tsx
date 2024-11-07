db91718907a6c0012a8bd3a803681f8c9b8b2e20
MiguelASFerreira <migant1306@gmail.com>
2024-11-06 21:20:54 -0300

    feat: add transcation type badge

---
+++ app/transactions/_columns/index.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Transaction } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/app/_components/ui/button";
+import { TrashIcon } from "lucide-react";
+import TransactionTypeBadge from "../_components/type-badge";
+
+export const transactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => (
+      <TransactionTypeBadge transaction={transaction} />
+    ),
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
+  },
+  {
+    accessorKey: "actions",
+    header: "Ações",
+    cell: () => {
+      return (
+        <div className="space-x-1">
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <TrashIcon />
+          </Button>
+        </div>
+      );
+    },
+  },
+];
