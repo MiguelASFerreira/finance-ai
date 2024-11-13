8cc5c87f21ed274f58b9a0e07b27b0923d593d28
MiguelASFerreira <migant1306@gmail.com>
2024-11-12 21:22:04 -0300

    refactor: ensure decimal warning does not happen

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -13,23 +13,25 @@ const TransactionsPage = async () => {
   if (!userId) {
     redirect("/login");
   }
-
   const transactions = await db.transaction.findMany({
     where: {
       userId,
+    },
+    orderBy: {
+      date: "desc",
     },
   });
   const userCanAddTransaction = await canUserAddTransaction();
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
-        {/*TITULO E BOTÃO*/}
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
+        {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
-        <ScrollArea>
+        <ScrollArea className="h-full">
           <DataTable
             columns={transactionColumns}
             data={JSON.parse(JSON.stringify(transactions))}
